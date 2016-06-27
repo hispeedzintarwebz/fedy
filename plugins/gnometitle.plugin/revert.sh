f8eaff5ccb2f0d3b0bdc261b23ec190c07029662
Andrew Turngren <andrewturngren@gmail.com>
2016-06-27 18:57:55 -0400

    revert script fixed

--- plugins/gnometitle.plugin/revert.sh
+++ plugins/gnometitle.plugin/revert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #remove lines with matching string "addedbyfedy" and output new file
-grep -vwE "addedbyfedy" ~/.config/gtk-3.0/gtk.css > ~/.config/gtk-3.0/gtk.css
+sed -i '/addedbyfedy/d' ~/.config/gtk-3.0/gtk.css
 
 #restart gnome shell
 gnome-shell -r &
