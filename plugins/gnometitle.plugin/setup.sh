e8e9c68af2fd7fb10a1aea8387e39b78856db6eb
Andrew Turngren <andrewturngren@gmail.com>
2016-06-27 18:43:55 -0400

    worked on css

--- plugins/gnometitle.plugin/setup.sh
+++ plugins/gnometitle.plugin/setup.sh
@@ -5,21 +5,26 @@ touch ~/.config/gtk-3.0/gtk.css
 
 #append css overrides
 cat <<EOF >> ~/.config/gtk-3.0/gtk.css
-.header-bar.default-decoration {
-        padding-top: 0px;
-        padding-bottom: 0px;
-    }
-
-.header-bar.default-decoration .button.titlebutton {
-    padding-top: 0px;
-    padding-bottom: 0px;
-}
-
-/* No line below the title bar */
-.ssd .titlebar {
-    border-width: 0;
-    box-shadow: none;
-}
+headerbar { /*addedbyfedy*/
+    min-height: 38px; /*addedbyfedy*/
+    padding-left: 2px; /*addedbyfedy*/
+    padding-right: 2px; /*addedbyfedy*/
+} /*addedbyfedy*/
+headerbar entry, /*addedbyfedy*/
+headerbar spinbutton, /*addedbyfedy*/
+headerbar button, /*addedbyfedy*/
+headerbar separator { /*addedbyfedy*/
+    margin-top: 2px; /*addedbyfedy*/
+    margin-bottom: 2px; /*addedbyfedy*/
+} /*addedbyfedy*/
+.default-decoration { /*addedbyfedy*/
+    min-height: 0; /*addedbyfedy*/
+    padding: 2px /*addedbyfedy*/
+} /*addedbyfedy*/
+.default-decoration .titlebutton { /*addedbyfedy*/
+    min-height: 26px; /*addedbyfedy*/
+    min-width: 26px; /*addedbyfedy*/
+} /*addedbyfedy*/
 EOF
 
 #restart gnome shell
